19249b289f4a0972222cf12d3fa2e510c843b964
Julien Gracia <gracia.julien@gmail.com>
2020-04-28 19:36:22 +0200

    Add a test to download choco

--- install_windows.ps1
+++ install_windows.ps1
@@ -1,8 +1,16 @@
-@powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
-
 $Packages = 'googlechrome', 'geforce-experience', 'steam', 'origin', 'git', 'keepass', 'discord', 'vscode', 'python3', 'eclipse', 'openjdk8', 'hwmonitor', 'racket'
 
-ForEach ($PackageName in $Packages)
-{
-    choco install $PackageName -y
+Function installPackages($Packages) {
+    ForEach ($PackageName in $Packages)
+    {
+        choco install $PackageName -y
+    }
+}
+
+If(Test-Path -Path "$env:ProgramData\Chocolatey") {
+    installPackages($Packages);
+} else {
+    Set-ExecutionPolicy Bypass -Scope Process -Force; `
+    Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+    installPackages($Packages);
 }
