13c0699454e0dedf1b9e6c5105796e2684a06cbc
Julien Gracia <gracia.julien@gmail.com>
2022-12-04 10:23:36 +0100

    Nvim: Typescript LSP

--- nvim/.config/nvim/lua/plugins.lua
+++
@@ -1,9 +0,0 @@
-vim.cmd [[packadd packer.nvim]]
-
-return require('packer').startup(function(use)
-    -- Packer can manager itself
-    use 'wbthomason/packer.nvim'
-
-    -- Dracula theme
-    use 'dracula/vim'
-end)
