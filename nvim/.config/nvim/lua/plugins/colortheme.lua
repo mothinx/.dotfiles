82d5d2ec999d89237cb41fa69eed321ab3044487
Julien Gracia <gracia.julien@gmail.com>
2024-11-22 18:27:13 +0100

    neovim: Add a lot of plugins, maybe too much

--- nvim/.config/nvim/lua/plugins/colortheme.lua
+++ nvim/.config/nvim/lua/plugins/colortheme.lua
@@ -1,9 +1,9 @@
-return { 
-	"catppuccin/nvim", 
-	name = "catppuccin", 
+return {
+	"catppuccin/nvim",
+	name = "catppuccin",
 	lazy = false,
 	priority = 1000,
 	config = function()
-		vim.cmd.colorscheme "catppuccin-macchiato"	
-	end
+		vim.cmd.colorscheme("catppuccin-macchiato")
+	end,
 }
---
+++ nvim/.config/nvim/lua/plugins/todo-comments.lua
@@ -0,0 +1,8 @@
+return {
+	-- Highligh todo, notes, etc in comments
+	-- TODO:
+	"folke/todo-comments.nvim",
+	event = "VimEnter",
+	dependencies = { "nvim-lua/plenary.nvim" },
+	opts = { signs = false },
+}
