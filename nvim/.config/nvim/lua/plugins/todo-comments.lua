c95229d05f4ccea40b27d403759ba7ea1618adc6
Julien Gracia <gracia.julien@gmail.com>
2025-02-26 19:36:18 +0100

    neovim: LazyVim starter

--- nvim/.config/nvim/lua/plugins/todo-comments.lua
+++
@@ -1,8 +0,0 @@
-return {
-	-- Highligh todo, notes, etc in comments
-	-- TODO:
-	"folke/todo-comments.nvim",
-	event = "VimEnter",
-	dependencies = { "nvim-lua/plenary.nvim" },
-	opts = { signs = false },
-}
