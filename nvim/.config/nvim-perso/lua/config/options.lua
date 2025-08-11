89e5652ca0751b80956d110b80ec3dd89b1407e6
Julien Gracia <gracia.julien@gmail.com>
2025-08-11 17:38:14 +0200

    nvim: An other config in progress

---
+++ nvim/.config/nvim-perso/lua/config/options.lua
@@ -0,0 +1,23 @@
+vim.g.mapleader = ' '
+vim.g.maplocalleader = "\\"
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.tabstop = 4 
+vim.opt.shiftwidth = 4 
+vim.opt.expandtab = true 
+vim.opt.incsearch = true 
+vim.opt.hlsearch = true 
+vim.opt.ignorecase = true 
+vim.opt.smartcase = true 
+vim.opt.termguicolors = true 
+vim.opt.showmode = true 
+vim.opt.scrolloff = 8
+vim.opt.wrap = false 
+vim.opt.backup = false 
+vim.opt.swapfile = false 
+vim.opt.undofile = true 
+
+
+
+
