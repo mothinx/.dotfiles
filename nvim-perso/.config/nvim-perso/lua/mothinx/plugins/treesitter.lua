aeacff3e7e193d746ccb6a1fd5bb798cf0451f4b
Julien Gracia <gracia.julien@gmail.com>
2025-11-21 21:29:14 +0100

    nvim-treesitter: add bash

--- nvim-perso/.config/nvim-perso/lua/mothinx/plugins/treesitter.lua
+++ nvim-perso/.config/nvim-perso/lua/mothinx/plugins/treesitter.lua
@@ -1,6 +1,6 @@
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all" (the listed parsers MUST always be installed)
-  ensure_installed = { "lua", "vim", "vimdoc", "query", "typescript", "python", "markdown", "markdown_inline", "diff", "html" },
+  ensure_installed = { "bash", "lua", "vim", "vimdoc", "query", "typescript", "python", "markdown", "markdown_inline", "diff", "html" },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
