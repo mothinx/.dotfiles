1ffda16dbf8ebcc5ef85e9f146ebd641526c9aa9
Julien Gracia <gracia.julien@gmail.com>
2026-01-09 08:29:27 +0100

    neovim: some fix

--- nvim/.config/nvim/lua/mothinx/plugins/cmp.lua
+++ nvim/.config/nvim/lua/mothinx/plugins/cmp.lua
@@ -1,18 +1,27 @@
 return {
   -- nvim-cmp, the completion engine
-  {
-    "hrsh7th/nvim-cmp",
-    event = "InsertEnter",
-    dependencies = {
-      "hrsh7th/cmp-nvim-lsp",
-      "hrsh7th/cmp-buffer",
-      "hrsh7th/cmp-path",
-      "Saghen/blink.cmp",
-    },
+   {
+     "hrsh7th/nvim-cmp",
+     event = "InsertEnter",
+     dependencies = {
+       "hrsh7th/cmp-nvim-lsp",
+       "hrsh7th/cmp-buffer",
+       "hrsh7th/cmp-path",
+       {
+         "Saghen/blink.cmp",
+         version = '1.*',
+       },
+     },
     config = function()
       local cmp = require("cmp")
       -- blink.cmp setup
-      require("blink.cmp").setup()
+      require("blink.cmp").setup({
+        fuzzy = {
+          prebuilt_binaries = {
+            force_version = 'v1.8.0'
+          }
+        }
+      })
       cmp.setup({
         sources = cmp.config.sources({
           { name = "nvim_lsp" },
--- nvim/.config/nvim/lua/mothinx/plugins/themes.lua
+++ nvim/.config/nvim/lua/mothinx/plugins/themes.lua
@@ -1,12 +1,13 @@
 return {
-  -- Theme manager
-  {
-    "EskelinenAntti/omarchy-theme-loader.nvim",
-    -- No specific config, it should pick up the themes automatically
-  },
-
-  -- Themes for the loader
-  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-  { "ellisonleao/gruvbox.nvim", priority = 1000 },
-  { "neanias/everforest-nvim", priority = 1000 },
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 1000,
+		config = function()
+			require("catppuccin").setup({
+				flavour = "mocha", -- latte, frappe, macchiato, mocha
+			})
+			vim.cmd.colorscheme("catppuccin")
+		end,
+	},
 }
